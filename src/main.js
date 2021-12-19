6eabcfb2ae6692265bf3bc7169498b7ca84ffbe1
Thorsten von Eicken <tve@voneicken.com>
2021-12-18 20:33:37 -0500

    More deployment.txt changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-/*
-  main.js - the main sensorgnome service which launches data acquisition according to
-  a stored program.  This service is restarted every 60 seconds (as set in its systemd
-  file) if it stops or fails.
-*/
+// main.js - the main sensorgnome service which runs data acquisition
+
+const DATADIR     = "/data/SGdata"                 // where data files are located
+const CONFDIR     = "/data/config"                 // where config files are located
+const DATAFILE    = CONFDIR+"/datafiles.json"      // where database about data files is located
+const DEPLOYMENT  = CONFDIR+"/deployment.txt"      // SensorGnome deployment info
+const ACQUISITION = CONFDIR+"/acquisition.txt"    // Receiver/sensor configuration
 
 // process.on("uncaughtException", function(err) {
 //      console.log('Caught exception: ' + err);
@@ -12,16 +14,16 @@ var doneQuit = false;
 
 function quitProcess () {
     if (doneQuit)
-        return;
-    doneQuit = true;
-    console.log("Exiting from main.js!\n");
-    TheMatron.emit("quit");
-    setTimeout(function() {console.log("Bye"); process.exit(0)}, 3000);
-};
+        return
+    doneQuit = true
+    console.log("Exiting from main.js!\n")
+    TheMatron.emit("quit")
+    setTimeout(function() {console.log("Bye"); process.exit(1)}, 3000)
+}
 
-process.on("SIGTERM", quitProcess);
-process.on("SIGQUIT", quitProcess);
-process.on("exit", quitProcess);
+process.on("SIGTERM", quitProcess)
+process.on("SIGQUIT", quitProcess)
+process.on("exit", quitProcess)
 
 Fs            = require('fs');
 Path          = require('path');
@@ -31,16 +33,17 @@ Net           = require('net');
 Events        = require('events');
 Zlib          = require('zlib');
 
-// information about the unit we're running on
-// (machine ID and bootcount)
-
-Machine = require('./machine.js');
+// information about the system we're running on (machine ID and bootcount)
+Machine       = require('./machine.js')
+// load configuration
+var Config    = require("./config.js")
+Deployment    = new Config.Deployment(DEPLOYMENT)
+Acquisition   = new Config.Acquisition(ACQUISITION)
 
 // Matron is a global object on which all SensorGnome event listeners
 // are registered, and from which all SensorGnome events are emitted.
-
-Matron = require('./matron.js');
-TheMatron = new Matron.Matron();
+Matron        = require('./matron.js');
+TheMatron     = new Matron.Matron();
 
 // Load singleton objects
 GPS           = new (require('./gps.js'))(TheMatron);
@@ -59,17 +62,6 @@ CornellTagXCVR= require("./cornelltagxcvr.js");
 
 //WavMaker      = require('./wavmaker.js');
 
-// Figure out the location of the deployment.txt file
-Deployment = new (require("./deployment.js").Deployment)(
-    [
-        "/data/config/deployment.txt",  // new preferred location
-    ]);
-
-// replace "-" with "_" in deployment short label, so filenames
-// use "-" only for delimiting fields
-
-Deployment.shortLabel = Deployment.shortLabel.replace(/-/g,"_");
-
 TagFinder = new (require('./tagfinder.js').TagFinder)(
     TheMatron,
     "/usr/bin/find_tags_unifile",
@@ -77,77 +69,64 @@ TagFinder = new (require('./tagfinder.js').TagFinder)(
         "/data/config/SG_tag_database.sqlite",  // new preferred location
         "/data/config/SG_tag_database.csv"],
     Deployment.module_options.find_tags.params
-);
+)
 
-DataSaver     = new (require('./datasaver.js').DataSaver) (TheMatron);
-DataFiles     = new (require('./datafiles.js'))(TheMatron);
+// Start the data file saving/writing/etc...
+DataSaver     = new (require('./datasaver.js').DataSaver) (TheMatron, DATADIR)
+DataFiles     = new (require('./datafiles.js').DataFiles) (TheMatron, DATADIR, DATAFILE)
+SafeStream    = require('./safestream.js').SafeStream
+// Create the two datafiles we write to for Lotek and CTT detections
+// Rotate every hour and also if hitting 1MB in size
+AllOut        = new SafeStream(TheMatron, "all", ".txt", 1000000, 300, "parse") // 1MB max filesize
+LifetagOut    = new SafeStream(TheMatron, "ctt", ".txt", 1000000, 3600, "parse")
 
-SafeStream    = require('./safestream.js').SafeStream;
-
-AllOut = new SafeStream(TheMatron, "all", ".txt", 1000000, 3600, "parse");
-
-LifetagOut = new SafeStream(TheMatron, "ctt", ".txt", 1000000, 3600, "parse");
-
-Uploader = new (require('./uploader.js').Uploader) (TheMatron);
-
-Relay = new (require('./relay.js').Relay) (TheMatron, 59000);
-
+//Uploader = new (require('./uploader.js').Uploader) (TheMatron);
+//Relay = new (require('./relay.js').Relay) (TheMatron, 59000);
 
 var clockNotSet = true;
 
-function do_nothing(err, stdout, stderr) {
-};
-
+// Propagate GPS fix info into data files
 TheMatron.on("gotGPSFix", function(fix) {
     let line = "G," + fix.time + "," + fix.lat + "," + fix.lon + "," + fix.alt + "\n"
     AllOut.write(line)
     LifetagOut.write(line)
     //ugly hack to set date from gps if gps has fix but system clock not set
     if (clockNotSet && (new Date()).getFullYear() < 2013) {
-        console.log("Trying to set time to " + fix.time + "\n");
-        ChildProcess.exec("date --utc -s @" + fix.time, do_nothing);
-        clockNotSet = false;
+        console.log("Trying to set time to " + fix.time + "\n")
+        ChildProcess.exec("date --utc -s @" + fix.time, ()=>{})
+        clockNotSet = false
     }
-});
+})
 
-TheMatron.on("vahData", function(d) {
-    AllOut.write(d);
-});
+// Propagate input received from vamp-alsa-host, i.e. Lotek pulses, to data file
+TheMatron.on("vahData", (d) => { AllOut.write(d) })
+// Propagate vah setting commands into data file
+TheMatron.on("setParam", (s) => {
+    AllOut.write(["S", s.time, s.port, s.par, s.val, s.errCode, s.err].join(',') + "\n")
+})
+// Propagate gps fixes to all data files
+TheMatron.on("gpsSetClock", (prec, elapsed) => {
+    const line = ["C", Date.now() / 1000, prec, elapsed].join(',') + "\n"
+    AllOut.write(line)
+    LifetagOut.write(line)
+})
 
-TheMatron.on("setParam", function(s) {
-    AllOut.write("S," + s.time + "," + s.port + "," + s.par + "," + s.val + "," + s.errCode + "," + s.err + "\n");
-});
+//Uploader.start();
 
-TheMatron.on("gpsSetClock", function(prec, elapsed) {
-    let line = "C," + Date.now() / 1000 + "," + prec + "," + elapsed + "\n"
-    AllOut.write(line);
-    LifetagOut.write(line);
-});
-
-// Start the uploader
-
-Uploader.start();
-
-setTimeout(()=>DataFiles.start(), 2000); // wait for stuff to settle a bit before scanning media
+// After initial flurry settles, read in info about existing data files
+setTimeout(()=>{ DataFiles.start().then(()=>{}) }, 2000)
 
 // start the GPS reader
+GPS.start(Acquisition.gps.secondsBetweenFixes)
+Chrony.start()
 
-GPS.start(Deployment.acquire.gps.secondsBetweenFixes);
-Chrony.start();
-
-// Now that all listeners for devAdded events have been registered, we
-// can start HubMan.
-
-HubMan.start();
+// Now that all listeners for devAdded events have been registered, we can start HubMan.
+HubMan.start()
 
 // Start the webserver
-
-WebServer.start(FlexDash);
-FlexDash.start(WebServer);
-Dashboard.start();
+WebServer.start(FlexDash)
+FlexDash.start(WebServer)
+Dashboard.start()
 
 // Start the tagFinder
-
-TagFinder.start();
-
-// Start the relay, which can resend messages from the matron to an arbitrary port
+TagFinder.start()
