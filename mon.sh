ac98bb522c9633233425105ee65daf86cd367d91
Thorsten von Eicken <tve@voneicken.com>
2022-10-15 19:57:56 -0400

    move auth state to /var/lib

--- mon.sh
+++ mon.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# sudo npm install -g nodemon
+[[ -f /usr/bin/nodemon ]] || sudo npm install -g nodemon
 export NODE_ENV=production
 export NODE_PATH=/home/gnome/sensorgnome-control/src
 export LC_ALL="C.UTF-8"
